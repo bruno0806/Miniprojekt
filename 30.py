04c917a30987b1f99c2c96943213a111109e3737
bruno0806 <50626108+bruno0806@users.noreply.github.com>
2020-05-14 19:53:47 +0200

    Update 30.py

--- 30.py
+++ 30.py
@@ -3,7 +3,12 @@ import string
 
 n = int(input("Adja meg a mátrix sorainak számát: "))
 m = int(input("Adja meg a mátrix oszlopainak számát: "))
-x = np.chararray((n, m), 1, True)
+while True:
+    if n==m:
+        n = int(input("Adja meg a mátrix sorainak számát: "))
+        m = int(input("Adja meg a mátrix oszlopainak számát: "))
+    else:
+        break
 karakterek = np.array(list(string.ascii_lowercase))
 matrix = np.random.choice(karakterek, [n, m])
 keret = np.full((n + 2, m + 2), '*')
